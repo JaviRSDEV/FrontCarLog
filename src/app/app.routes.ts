d3e10abe84bb839c88012dd6a35f359393bb37c7
JaviRSDEV <jarase2004@gmail.com>
2026-03-03 15:05:57 +0100

    Commit inicial del proyecto, comenzada la construcción de la página de login de la web

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { CounterPageComponent } from './pages/counter/counter-page.components';
+
+export const routes: Routes = [
+
+  {
+    path: '',
+    component: CounterPageComponent
+  }
+];
