38a02be86f5967b8d53e6e40c496a4f212c5b5c6
JaviRSDEV <jarase2004@gmail.com>
2026-03-04 18:18:09 +0100

    Añadidas animaciones y completados los modales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './pages/counter/counter-page.components';
+import { Login } from './login/login';
 
 export const routes: Routes = [
 
   {
     path: '',
-    component: CounterPageComponent
+    component: Login
   }
 ];
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, Form } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
@@ -12,9 +13,29 @@ export class Login {
   showLogin = false;
   showRegister = false;
 
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
   openLogin(){this.showLogin = true; }
-  closeLogin(){this.showLogin = false; }
+  closeLogin(){
+    this.showLogin = false;
+    this.loginForm.reset()
+  }
 
   openRegister(){this.showRegister = true; }
   closeRegister(){this.showRegister = false; }
+
+  onLoginSubmit(){
+    if(this.loginForm.valid){
+      console.log("Datos listos para su envio al backend:", this.loginForm.value);
+    }else{
+      console.log("El formulario contiene errores");
+      this.loginForm.markAllAsTouched();
+    }
+  }
 }
