03546db57373e04f0b2f186f081bcbf4c1b61df4
JaviRSDEV <jarase2004@gmail.com>
2026-03-06 10:11:09 +0100

    Creados los componentes de nav, header y footer e integrados con exito en el dashboard layout

--- src/app/pages/dashboard-layout/dashboard-layout.ts
+++ src/app/pages/dashboard-layout/dashboard-layout.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from '../../components/shared/navbar/navbar';
+import { Header } from '../../components/shared/header/header';
+import { Footer } from '../../components/shared/footer/footer';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Header, Footer],
   templateUrl: './dashboard-layout.html',
   styleUrl: './dashboard-layout.css',
 })
