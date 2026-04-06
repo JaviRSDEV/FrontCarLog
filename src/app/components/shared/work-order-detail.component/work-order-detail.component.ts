b9dfed7c6859e3e8f63c22ec1aeb3571155546d7
JaviRSDEV <jarase2004@gmail.com>
2026-04-06 11:08:06 +0200

    Finalizada vista de ordenes de trabajo

---
+++ src/app/components/shared/work-order-detail.component/work-order-detail.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-order-detail.component',
+  imports: [],
+  templateUrl: './work-order-detail.component.html',
+  styleUrl: './work-order-detail.component.css',
+})
+export class WorkOrderDetailComponent {}
