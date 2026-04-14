e91a85076e0e6b157150d152cbd364c3395bdc21
JaviRSDEV <jarase2004@gmail.com>
2026-04-14 22:25:14 +0200

    added enviroments files

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -1,7 +1,9 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const isApiRequest = req.url.includes('http://localhost:8081');
+  const isApiRequest = req.url.startsWith(environment.apiUrl);
+
   if (isApiRequest) {
     const peticionClonada = req.clone({
       withCredentials: true,
