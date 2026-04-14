6003ba29a5757342196ae170b000af2bd6968728
JaviRSDEV <jarase2004@gmail.com>
2026-04-14 13:59:25 +0200

    minor changes, deleted a few logs

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -2,9 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const isApiRequest = req.url.includes('http://localhost:8081');
-  console.log('🕵️ Interceptor evaluando:', req.url); // 👈 Chivato 1
   if (isApiRequest) {
-    console.log('✅ Añadiendo credenciales a:', req.url); // 👈 Chivato 2
     const peticionClonada = req.clone({
       withCredentials: true,
     });
